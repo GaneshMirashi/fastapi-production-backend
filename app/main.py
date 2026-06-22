534376f0a227dfa6208d87b64a1b2ddc521759ea
GaneshMirashi <ganeshmirashi07@gmail.com>
2026-06-22 14:41:44 +0530

    postgresql connected with FastAPI.

--- app/main.py
+++ app/main.py
@@ -1,16 +1,20 @@
 from fastapi import FastAPI
 
 from app.core.config import settings
+from app.core.database import Base
+from app.core.database import engine
+
+from app.models import User
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
-    title=settings.APP_NAME,
-    version=settings.APP_VERSION
+title=settings.APP_NAME,
+version=settings.APP_VERSION
 )
-
 
 @app.get("/")
 async def root():
     return {
-        "message": "Backend Running",
-        "app_name": settings.APP_NAME
+    "message": "Backend Running Successfully"
     }
