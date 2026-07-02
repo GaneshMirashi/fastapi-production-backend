c5943f036a207a0954804b8368c1cffe686de77c
GaneshMirashi <ganeshmirashi07@gmail.com>
2026-07-02 17:03:18 +0530

    added code for logging and loggin_middleware.

--- app/main.py
+++ app/main.py
@@ -1,16 +1,27 @@
 from fastapi import FastAPI
 
+from app.api.v1.router import api_router
+
 from app.core.config import settings
-from app.core.database import Base
-from app.core.database import engine
+from app.core.exceptions import global_exception_handler
 
-from app.models import User
-
-# Base.metadata.create_all(bind=engine)
+from app.middleware.logging_middleware import LoggingMiddleware
 
 app = FastAPI(
 title=settings.APP_NAME,
 version=settings.APP_VERSION
+)
+
+app.add_middleware(LoggingMiddleware)
+
+app.add_exception_handler(
+Exception,
+global_exception_handler
+)
+
+app.include_router(
+api_router,
+prefix=settings.API_V1_PREFIX
 )
 
 @app.get("/")
